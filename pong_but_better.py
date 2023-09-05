1dbf682ccf23be3b95210bc686d5e1924f9341d4
unknown <brannongarrett77@gmail.com>
2023-09-05 13:57:30 +0200

    Added a blue ball and 2 red paddles

--- pong_but_better.py
+++ pong_but_better.py
@@ -9,8 +9,32 @@ pygame.display.set_caption("Pong But Better")
 
 run = True
 
+# Colors
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+# Ball
+radius = 15
+ball_x, ball_y = WIDTH / 2 - radius, HEIGHT / 2 - radius
+
+# Paddles
+paddle_width, paddle_height = 20, 120
+left_paddle_y = right_paddle_y = HEIGHT / 2 - paddle_height / 2
+left_paddle_x, right_paddle_x = 100 - paddle_width / 2, WIDTH - (100 - paddle_width / 2)
+
 # Main loop
 while run:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             run = False
+
+    pygame.draw.circle(wn, BLUE, (ball_x, ball_y), radius)
+    pygame.draw.rect(
+        wn, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height)
+    )
+    pygame.draw.rect(
+        wn,
+        RED,
+        pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height),
+    )
+    pygame.display.update()
