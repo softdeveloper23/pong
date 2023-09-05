f7e08cda42218a9789d967c1314e766445cd3f6f
unknown <brannongarrett77@gmail.com>
2023-09-05 13:45:40 +0200

    Added Pygame display and quit mode

--- pong_but_better.py
+++ pong_but_better.py
@@ -1,3 +1,16 @@
 import pygame
 
 pygame.init()
+
+# INITIALS
+WIDTH, HEIGHT = 1000, 600
+wn = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong But Better")
+
+run = True
+
+# Main loop
+while run:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            run = False
