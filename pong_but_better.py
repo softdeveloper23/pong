d43f08ef0ba6551fd5acee43e5ba0298bce91c58
unknown <brannongarrett77@gmail.com>
2023-09-06 17:52:01 +0200

    Added movement to the paddles

--- pong_but_better.py
+++ pong_but_better.py
@@ -23,6 +23,7 @@ ball_vel_x, ball_vel_y = 0.2, 0.2
 paddle_width, paddle_height = 20, 120
 left_paddle_y = right_paddle_y = HEIGHT / 2 - paddle_height / 2
 left_paddle_x, right_paddle_x = 100 - paddle_width / 2, WIDTH - (100 - paddle_width / 2)
+right_paddle_vel = left_paddle_vel = 0
 
 # Main loop
 while run:
@@ -30,6 +31,19 @@ while run:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             run = False
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_UP:
+                right_paddle_vel = -0.9
+            if i.key == pygame.K_DOWN:
+                right_paddle_vel = 0.9
+            if i.key == pygame.K_w:
+                left_paddle_vel = -0.9
+            if i.key == pygame.K_s:
+                left_paddle_vel = 0.9
+
+        if i.type == pygame.KEYUP:
+            right_paddle_vel = 0
+            left_paddle_vel = 0
 
     # Ball movement control
     if ball_y <= 0 + radius or ball_y >= HEIGHT - radius:
@@ -45,6 +59,8 @@ while run:
     # Movement
     ball_x += ball_vel_x
     ball_y += ball_vel_y
+    right_paddle_y += right_paddle_vel
+    left_paddle_y += left_paddle_vel
 
     # OBJECTS
     pygame.draw.circle(wn, BLUE, (ball_x, ball_y), radius)
