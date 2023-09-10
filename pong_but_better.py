fe385f55fd881f11c2953f381c0f7ce4622f24be
unknown <brannongarrett77@gmail.com>
2023-09-10 16:42:03 +0200

    Added code

--- pong_but_better.py
+++ pong_but_better.py
@@ -42,10 +42,14 @@ while run:
                 right_paddle_vel = -0.9
             if i.key == pygame.K_DOWN:
                 right_paddle_vel = 0.9
+            if i.key == pygame.K_RIGHT and right_gadget_remaining > 0:
+                right_gadget = 1
             if i.key == pygame.K_w:
                 left_paddle_vel = -0.9
             if i.key == pygame.K_s:
                 left_paddle_vel = 0.9
+            if i.key == pygame.K_d and left_gadget_remaining > 0:
+                left_gadget = 1
 
         if i.type == pygame.KEYUP:
             right_paddle_vel = 0
@@ -114,6 +118,8 @@ while run:
         if right_paddle_y <= ball_y <= right_paddle_y + paddle_height:
             ball_x = right_paddle_x
             ball_vel_x *= -1
+
+    # Gadgets in action
 
     # Movement
     ball_x += ball_vel_x
